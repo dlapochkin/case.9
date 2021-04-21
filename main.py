ca0c46f82ae17a7529c6b77642efe680ef1ac987
dkrivoshapova <d.krivoshapova@g.nsu.ru>
2021-04-21 14:42:42 +0700

    up down menu

--- main.py
+++ main.py
@@ -1,18 +1,26 @@
 import os
 def main():
     print(os.getcwd())
-    function=input('''1.Промотр каталога
-    2.На уровень вверх
-    3.На уровень вниз
-    4.Количество файлов и каталогов
-    5.Размер текущего файла в байтах
-    6.Поиск файла
-    7.Выход из пргораммы
-    Выберите пункт меню:
-    ''')
-    if funtion==2:
+    function=acceptCommand()
+    runCommand(function)
+def acceptCommand():
+    function = int(input('''1.Промотр каталога
+2.На уровень вверх
+3.На уровень вниз
+4.Количество файлов и каталогов
+5.Размер текущего файла в байтах
+6.Поиск файла
+7.Выход из пргораммы
+Выберите пункт меню:
+'''))
+    if function in (1,2,3,4,5,6,7):
+        return function
+    acceptCommand()
+
+def runCommand(command):
+    if command==2:
         moveUp()
-    elif function==3:
+    elif command==3:
         moveDown(os.getcwd())
 
 def moveUp():
@@ -22,10 +30,11 @@ def moveUp():
     os.chdir(UpDir)
     print(os.getcwd())
 def moveDown(currentDir):
-    name=input()
+    name=input('Ведите имя подкаталога:')
     new_name=currentDir+r'\\'+ name
     if os.path.exists(new_name):
         os.chdir(new_name)
         print(os.getcwd())
     else:
         print('Такого файла или папки не существует')
+main()
