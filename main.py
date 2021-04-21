b2ad652715778fecdcbc9cb86c5063659338b1f6
ToliaKuznetsov <73088456+ToliaKuznetsov@users.noreply.github.com>
2021-04-21 15:25:24 +0700

    test

--- main.py
+++ main.py
@@ -53,4 +53,14 @@ def moveDown(currentDir):
     runCommand(acceptCommand())
 
 
+def countBytes(folder):
+    total_size = os.path.getsize(folder)
+    for item in os.listdir(folder):
+        itempath = os.path.join(folder, item)
+        if os.path.isfile(itempath):
+            total_size += os.path.getsize(itempath)
+        elif os.path.isdir(itempath):
+            total_size += countBytes(itempath)
+    return total_size
+
 main()
