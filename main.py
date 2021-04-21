0a488ba786c1d8e0639cc7a26ddc4fea75037f7f
ToliaKuznetsov <73088456+ToliaKuznetsov@users.noreply.github.com>
2021-04-21 15:15:41 +0700

    test

--- main.py
+++ main.py
@@ -1,16 +1,56 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import os
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def main():
+    print(os.getcwd())
+    function = acceptCommand()
+    runCommand(function)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def acceptCommand():
+    function = int(input('''1.Промотр каталога
+2.На уровень вверх
+3.На уровень вниз
+4.Количество файлов и каталогов
+5.Размер текущего файла в байтах
+6.Поиск файла
+7.Выход из пргораммы
+Выберите пункт меню:
+'''))
+    if function in (1, 2, 3, 4, 5, 6, 7):
+        return function
+    acceptCommand()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+def runCommand(command):
+    if command == 2:
+        moveUp()
+    elif command == 3:
+        moveDown(os.getcwd())
+    elif command==7:
+        print('Спасибо за использование нашей программы')
+        exit()
+
+
+
+def moveUp():
+    currentDir = os.getcwd()
+    print(os.getcwd())
+    UpDir = currentDir[:currentDir.rfind('\\')]
+    os.chdir(UpDir)
+    print(os.getcwd())
+    runCommand(acceptCommand())
+
+
+def moveDown(currentDir):
+    name = input('Ведите имя подкаталога:')
+    new_name = currentDir + r'\\' + name
+    if os.path.exists(new_name):
+        os.chdir(new_name)
+        print(os.getcwd())
+    else:
+        print('Такого файла или папки не существует')
+    runCommand(acceptCommand())
+
+
+main()
