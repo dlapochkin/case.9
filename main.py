cbc6c0651eda6814c804e5d3652b9d2d33440549
dkrivoshapova <d.krivoshapova@g.nsu.ru>
2021-04-21 10:25:07 +0700

    UP DOWN

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+import os
+def moveUp():
+    currentDir=os.getcwd()
+    print(os.getcwd())
+    UpDir= currentDir[:currentDir.rfind('\\')]
+    os.chdir(UpDir)
+    print(os.getcwd())
+def moveDown(currentDir):
+    name=input()
+    new_name=currentDir+r'\\'+ name
+    if os.path.exists(new_name):
+        os.chdir(new_name)
+        print(os.getcwd())
+    else:
+        print('Такого файла или папки не существует')
