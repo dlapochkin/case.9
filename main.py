8211edede1063aa8862321421ec98207ec6d213d
dkrivoshapova <d.krivoshapova@g.nsu.ru>
2021-04-21 14:54:40 +0700

    up down menu

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import os
+
+
 def main():
     print(os.getcwd())
-    function=acceptCommand()
+    function = acceptCommand()
     runCommand(function)
+
+
 def acceptCommand():
     function = int(input('''1.Промотр каталога
 2.На уровень вверх
@@ -13,28 +17,37 @@ def acceptCommand():
 7.Выход из пргораммы
 Выберите пункт меню:
 '''))
-    if function in (1,2,3,4,5,6,7):
+    if function in (1, 2, 3, 4, 5, 6, 7):
         return function
     acceptCommand()
 
+
 def runCommand(command):
-    if command==2:
+    if command == 2:
         moveUp()
-    elif command==3:
+    elif command == 3:
         moveDown(os.getcwd())
+    
+
 
 def moveUp():
-    currentDir=os.getcwd()
+    currentDir = os.getcwd()
     print(os.getcwd())
-    UpDir= currentDir[:currentDir.rfind('\\')]
+    UpDir = currentDir[:currentDir.rfind('\\')]
     os.chdir(UpDir)
     print(os.getcwd())
+    runCommand(acceptCommand())
+
+
 def moveDown(currentDir):
-    name=input('Ведите имя подкаталога:')
-    new_name=currentDir+r'\\'+ name
+    name = input('Ведите имя подкаталога:')
+    new_name = currentDir + r'\\' + name
     if os.path.exists(new_name):
         os.chdir(new_name)
         print(os.getcwd())
     else:
         print('Такого файла или папки не существует')
+    runCommand(acceptCommand())
+
+
 main()
