8a99b296106095a5e7cbea4304460dc8695253af
ToliaKuznetsov <73088456+ToliaKuznetsov@users.noreply.github.com>
2021-04-21 15:11:56 +0700

    test

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ list = os.listdir(os.getcwd())
 number_files = len(list)
 print (number_files)
 print(os.getcwd())
-print(os.listdir())
+#print(os.listdir())
 
 pdir = 'C:/Users/gaga7/PycharmProjects/case.9'
 contdir = []
@@ -41,3 +41,48 @@ for i in contdir:
    i = str(i)
    end_list.append(i[i.find(',')+1:-1])
 print(end_list)
+
+
+def folder_size(path='.'):
+   total = 0
+   for entry in os.scandir(path):
+      if entry.is_file():
+         total += entry.stat().st_size
+      elif entry.is_dir():
+         total += folder_size(entry.path)
+   return total
+
+#считает колчество байт
+def countBytes(folder):
+    total_size = os.path.getsize(folder)
+    for item in os.listdir(folder):
+        itempath = os.path.join(folder, item)
+        #print(folder, item)
+        if os.path.isfile(itempath):
+            total_size += os.path.getsize(itempath)
+        elif os.path.isdir(itempath):
+            total_size += countBytes(itempath)
+    return total_size
+
+
+#print (str(countBytes(".git")))
+
+def countFiles(folder):
+   total_file = os.path.getsize(folder)
+
+   for item in os.listdir(folder):
+      itempath = os.path.join(folder, item)
+      if os.path.isfile(itempath):
+         total_file += os.path.getsize(itempath)
+      elif os.path.isdir(itempath):
+         total_file += countFiles(itempath)
+         print(countFiles(itempath))
+   return total_file
+
+
+for root, directories, files in os.walk(r"os.getcwd()"):
+    print(root)
+    for directory in directories:
+        print(directory)
+    for file in files:
+        print(file)
