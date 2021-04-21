9073e2fbc954cb45d8697935715d04c3377acf17
ToliaKuznetsov <73088456+ToliaKuznetsov@users.noreply.github.com>
2021-04-21 10:35:02 +0700

    test

--- main.py
+++ main.py
@@ -1,16 +1,43 @@
-# This is a sample Python script.
+import os, os.path
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+'''root_path = '/загрузки/Telegram Desktop/'
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+for root, dirs, files in os.walk(root_path):
+   for name in files:
+      print(os.path.join(root, name))
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+for address, dirs, files in folder:
+   for file in files:
+      print(address+'/'+file)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+def main():
+   while True:
+      print(os.getcwd())
+      print('MENU')
+      command = acceptCommand()
+      runCommand(command)
+      if command == QUIT:
+         print('Работа программы завершена.')
+         break
+folder = []
+for i in os.walk('test'):
+   folder.append(i)
+print(folder)'''
+#print (len(['PycharmProjects' for 'PycharmProjects' in os.listdir('.') if os.path.isfile('PycharmProjects')]))
+list = os.listdir(os.getcwd())
+number_files = len(list)
+print (number_files)
+print(os.getcwd())
+print(os.listdir())
+
+pdir = 'C:/Users/gaga7/PycharmProjects/case.9'
+contdir = []
+end_list= []
+for i in os.walk(pdir):
+   contdir.append(i)
+for i in contdir:
+   i = str(i)
+   end_list.append(i[i.find(',')+1:-1])
+print(end_list)
